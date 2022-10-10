8383eff3f2916ef64981c7d8b0148104639e9a4d
guillaume.gonnet-dev <guillaume.gonnet.dev@gmail.com>
2022-10-10 08:47:33 -0400

    remove unused file

--- demineur/demineur_fr_FR.ts
+++ demineur/demineur_fr_FR.ts
@@ -5,79 +5,66 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="125"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="47"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="146"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="56"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="147"/>
         <source>Option</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="70"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="126"/>
         <source>New</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="73"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="128"/>
         <source>Ctrl+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="82"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="130"/>
         <source>Load last game</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="85"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="132"/>
         <source>Load</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="88"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="135"/>
         <source>Ctrl+O</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="97"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="137"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="100"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="139"/>
         <source>Ctrl+S</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="109"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="141"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="112"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="143"/>
         <source>Ctrl+Q</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="117"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_mainwindow.h" line="145"/>
         <source>Grid Size</source>
         <translation type="unfinished"></translation>
     </message>
@@ -86,31 +73,26 @@
     <name>OptionDialog</name>
     <message>
         <location filename="optiondialog.ui" line="14"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_optiondialog.h" line="120"/>
         <source>Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="optiondialog.ui" line="26"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_optiondialog.h" line="121"/>
         <source>Options</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="optiondialog.ui" line="63"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_optiondialog.h" line="122"/>
         <source>Nb boxes wide</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="optiondialog.ui" line="81"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_optiondialog.h" line="123"/>
         <source>Nb boxes height</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="optiondialog.ui" line="99"/>
-        <location filename="../build-demineur-Desktop-Debug/demineur_autogen/include/ui_optiondialog.h" line="124"/>
         <source>Nb mines</source>
         <translation type="unfinished"></translation>
     </message>
